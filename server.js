9e540e5ddc4c77983f7929c6ae0fe59d14c6f423
Hillary Stewart <hillarystewart4427@gmail.com>
2019-03-17 11:23:57 -0600

    Add more endpoints

--- server.js
+++ server.js
@@ -21,8 +21,37 @@ app.get('/api/v1/cohorts', (request, response) => {
     })
 })
 
+app.get('/api/v1/cohorts/:id', (request, response) => {
+  database('cohorts').select()
+    .then((cohorts) => {
+      response.status(200).json(cohorts)
+    })
+    .catch((error) => {
+      response.status(500).json({ error })
+    })
+})
+
+app.get('/api/v1/students', (request, response) => {
+  database('students').select()
+    .then((students) => {
+      response.status(200).json(students)
+    })
+    .catch((error) => {
+      response.status(500).json({ error })
+    })
+})
+
+app.get('/api/v1/students/:id', (request, response) => {
+  database('students').select()
+    .then((students) => {
+      response.status(200).json(students)
+    })
+    .catch((error) => {
+      response.status(500).json({ error })
+    })
+})
+
 app.post('/api/v1/cohorts', (request, response) => {
-  console.log(request.body)
   const cohort = request.body;
 
   for (let requiredParam of ['cohort_num', 'current_mod', 'lead_instructor']) {
@@ -47,3 +76,35 @@ app.post('/api/v1/cohorts', (request, response) => {
       response.status(500).json({ error });
     });
 });
+
+app.post('/api/v1/students', (request, response) => {
+  console.log(request.body)
+  const student = request.body;
+  let requiredParams = ['pronouns', 'first_name', 'last_name', 'cohort_id'];
+  requiredParams.forEach(param => {
+    if (!student[param]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: 
+        { 
+          pronouns: <String>, 
+          first_name: <String>, 
+          last_name: <String>,
+          cohort_id: <Integer>
+        }. You're missing a "${requiredParam}" property.` 
+      });
+    }
+  })
+
+  database('students').insert(student, 'id')
+    .then(student => {
+      response.status(201).json({ id: student[0] })
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
+app.delete('/api/v1/students/:id', (request, response) => {
+
+})
