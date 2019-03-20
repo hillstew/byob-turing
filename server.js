618c516698da3a73164d02e5ce4a6ec5aefc3926
Hillary Stewart <hillarystewart4427@gmail.com>
2019-03-20 10:55:48 -0600

    Add cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+app.use(cors());
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
