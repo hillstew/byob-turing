caba76bd0b29b2eccecd7e08433d029c5b3b8bba
Hillary Stewart <hillarystewart4427@gmail.com>
2019-03-20 11:05:42 -0600

    Add cors var

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 const environment = process.env.NODE_ENV || 'development';
